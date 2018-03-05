3fbfe09babad49c0f95d64584f05b6f6aa0006d4
babukhan <b.khan@emtyaztech.com>
2018-03-05 16:36:44 +0500

    company registered successfully
    company registered successfully

--- pms/src/java/dao/CompaniesDAO.java
+++ pms/src/java/dao/CompaniesDAO.java
@@ -5,10 +5,38 @@
  */
 package dao;
 
+import entity.Company;
+import java.sql.SQLException;
+import util.SQLQueryUtil;
+
 /**
  *
  * @author babu
  */
 public class CompaniesDAO {
+    
+    public int registerCompany(Company company) {
+        int rowsAffected = 0;
+        
+        SQLQueryUtil sql = new SQLQueryUtil();
+        sql.connect(false);
+        
+        String queryInsert = "INSERT INTO pms_schema.companies (company_name, address, phone_number) " +
+"values('" + company.getCompanyName() + "', '" + company.getAddress() + "', '" + company.getPhoneNumber() + "');";
+        
+        System.out.println(queryInsert);
+        
+        try {
+            rowsAffected = sql.executeUpdate(queryInsert);
+            sql.commit();
+            System.out.println("Company registered successfully.");
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            sql.disconnect();
+        }
+        
+        return rowsAffected;
+    }
     
 }
--- pms/src/java/services/CompaniesService.java
+++ pms/src/java/services/CompaniesService.java
@@ -5,18 +5,54 @@
  */
 package services;
 
+import dao.CompaniesDAO;
 import entity.Company;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import util.SQLQueryUtil;
 
 /**
  *
  * @author babu
  */
 public class CompaniesService {
+    
+    private CompaniesDAO companiesDAO;
+    
+   public CompaniesService() {
+        companiesDAO = new CompaniesDAO();
+   }
    
-    public boolean registerCompany(Company company) {
+    public int registerCompany(Company company) {
+        int rowsAffected = 0;
+        int count = 0;
+        
+        SQLQueryUtil sql = new SQLQueryUtil();
+        sql.connect(false);
+        
         System.out.println(company.getCompanyName());
         System.out.println(company.getAddress());
         System.out.println(company.getPhoneNumber());
-        return false;
+        
+        String queryCheck = "SELECT count(*) as count from pms_schema.companies where company_name like('" + company.getCompanyName() + "');";
+        
+        try {
+            ResultSet resultSet = sql.executeQuery(queryCheck);
+            resultSet.next();
+            
+            count = resultSet.getInt("count");
+            
+            if(count == 0) {
+                rowsAffected = companiesDAO.registerCompany(company);
+            }
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            sql.disconnect();
+        }
+
+        return rowsAffected;
+        
     }
+    
 }
