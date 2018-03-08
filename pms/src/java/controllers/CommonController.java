b1252ac5dbe995d71b474761425a4c43f65dff46
Abid Khan <kabid9876@gmail.com>
2018-03-08 15:21:37 +0500

    added list company
    added list company to combobox

--- pms/src/java/controllers/CommonController.java
+++ pms/src/java/controllers/CommonController.java
@@ -8,7 +8,9 @@ package controllers;
 import entity.Company;
 import java.io.Serializable;
 import java.util.List;
+import javax.faces.bean.CustomScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import services.CommonService;
 
@@ -16,18 +18,32 @@ import services.CommonService;
  *
  * @author babu
  */
-@ManagedBean(name = "commonController")
+@ManagedBean(name = "commonControllerBean")
 @ViewScoped
 
 public class CommonController implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    
+    @ManagedProperty(value = "#{anyCompany}")
+    Company company;
     CommonService commonService;
 
+    
+    
     public CommonController() {
         commonService = new CommonService();
     }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+    
+    
 
     public List<Company> getListCompanies() {
         return commonService.getListCompanies();
